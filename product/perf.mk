afdfd83820559675f7613b24648e3a4d4d8669e6
FireLord1 <firelord.xda@gmail.com>
2017-01-12 22:19:20 +0530

    msm8916-common: cleanup system.prop
    -remove lenovo things
    -move props from mk files to .prop file
    -add some props form tomato,osprey
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>
    
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- product/perf.mk
+++
@@ -1,6 +0,0 @@
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=4
-    ro.config.max_starting_bg=8 \
-    ro.sys.fw.use_trim_settings=true \
-    ro.product.hw.version=H201 \
