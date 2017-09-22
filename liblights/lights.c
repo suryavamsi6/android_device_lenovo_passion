crdroidandroid/device_lenovo_kuntao

kuntao: liblights: Set all color paths as one
* not sure if its gonna work but still trying..
  basic logic, if color codes are exactly same for
  specific color ex. red = something which our
  rgb/brightness needs then it might work.

Signed-off-by: FireLord1 <firelord.xda@gmail.com>

liblights/lights.c
@@ -42,25 +42,25 @@ static struct light_state_t g_battery;
 static int g_attention = 0;
 
 char const*const RED_LED_FILE
-        = "/sys/class/leds/red/brightness";
+        = "/sys/class/leds/rgb/brightness";
 
 char const*const GREEN_LED_FILE
-        = "/sys/class/leds/green/brightness";
+        = "/sys/class/leds/rgb/brightness";
 
 char const*const BLUE_LED_FILE
-        = "/sys/class/leds/blue/brightness";
+        = "/sys/class/leds/rgb/brightness";
 
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
 
 char const*const RED_BLINK_FILE
-        = "/sys/class/leds/red/blink";
+        = "/sys/class/leds/rgb/blink";
 
 char const*const GREEN_BLINK_FILE
-        = "/sys/class/leds/green/blink";
+        = "/sys/class/leds/rgb/blink";
 
 char const*const BLUE_BLINK_FILE
-        = "/sys/class/leds/blue/blink";
+        = "/sys/class/leds/rgb/blink";
 
 /**
  * device methods

suryavamsi6/android_device_lenovo_passion

Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

liblights/lights.c
@@ -42,13 +42,13 @@ static struct light_state_t g_battery;
 static int g_attention = 0;
 
 char const*const RED_LED_FILE
-        = "/sys/class/leds/red/brightness";
+        = "/sys/class/leds/rgbled/brightness";
 
 char const*const GREEN_LED_FILE
-        = "/sys/class/leds/green/brightness";
+        = "/sys/class/leds/rgbled/brightness";
 
 char const*const BLUE_LED_FILE
-        = "/sys/class/leds/blue/brightness";
+        = "/sys/class/leds/rgbled/brightness";
 
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
@@ -57,13 +57,13 @@ char const*const BUTTON_FILE
         = "/sys/class/leds/button-backlight/brightness";
 
 char const*const RED_BLINK_FILE
-        = "/sys/class/leds/red/blink";
+        = "/sys/class/leds/rgbled/blink";
 
 char const*const GREEN_BLINK_FILE
-        = "/sys/class/leds/green/blink";
+        = "/sys/class/leds/rgbled/blink";
 
 char const*const BLUE_BLINK_FILE
-        = "/sys/class/leds/blue/blink";
+        = "/sys/class/leds/rgbled/blink";
 
 /**
  * device methods